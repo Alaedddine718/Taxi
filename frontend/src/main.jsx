a4b0e38177bd7c2768493428a396695f9d8630e1
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-12-10 15:15:13 +0100

    cambio del main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,21 @@
-// frontend/src/main.jsx
+// PRUEBA TEMPORAL main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div
+    style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "2rem",
+      color: "white",
+    }}
+  >
+    HOLA, PRUEBA REACT
+  </div>
 );
+
 
 
